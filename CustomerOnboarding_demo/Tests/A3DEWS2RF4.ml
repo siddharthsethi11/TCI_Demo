32a2c1043c756ae4df3e05115eac140441de957e
siddharthsethi11 <sethisiddharth504@gmail.com>
2023-01-29 23:25:54 +0530

    First push

---
+++ CustomerOnboarding_demo/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/CustomerOnboarding/Tests/CustomerOnboarding1.bwt#/"/>
+  <EmulationDataFile>ACED0005740017437573746F6D65724F6E626F617264696E67312E627774</EmulationDataFile>
+</emulation:BWTFile>
